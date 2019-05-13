8540fb19aec27afd481b413692cfe0ee5a3abb5a
Ihor <mnushko@gmarkg.com>
2019-05-13 12:13:14 +0300

    Added pages for works

---
+++ resources/views/work/all.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="work box">
+        <item v-for="n in 10" :key="n">
+            <div class="image"></div>
+        </item>
+    </div>
+</template>
+
+<script>
+  import Item from './one'
+
+  export default {
+    name: 'all',
+    components: { Item }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/work/one.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="work__item">
+        <div class="image"></div>
+        <div class="image-bg"></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'one',
+    mounted () {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
