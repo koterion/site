309d64d97af9db601b99c7e2445f16a032804b48
koterion <koterion317@gmail.com>
2019-07-31 17:17:41 +0300

    Added slider for portfolio one

--- resources/views/portfolio/one.vue
+++ resources/views/portfolio/one.vue
@@ -1,27 +1,15 @@
 <template>
   <div class="portfolio__one box">
-    <div class="glide">
+    <div v-if="typeof images !== 'string'" class="glide">
       <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides">
-          <li class="glide__slide">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, nulla.</li>
-          <li class="glide__slide">Aperiam natus nostrum vel. Dolorem doloribus laudantium mollitia repellat ut.</li>
-          <li class="glide__slide">Ab dolorem, esse expedita harum laudantium natus ratione rem sunt?</li>
-          <li class="glide__slide">At consequuntur, dolorum maxime nihil officiis porro quisquam saepe voluptatibus?</li>
-          <li class="glide__slide">At, commodi cumque deserunt earum esse nisi provident quos sunt.</li>
-          <li class="glide__slide">Accusamus accusantium blanditiis dolorem eaque error facilis possimus sequi veniam!</li>
-        </ul>
-      </div>
-      <div class="glide__arrows" data-glide-el="controls">
-        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
-        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+        <div class="glide__slides">
+          <figure v-for="(image, index) in images" class="glide__slide">
+            <img :src="image" :alt="index">
+          </figure>
+        </div>
       </div>
       <div class="glide__bullets" data-glide-el="controls[nav]">
-        <button class="glide__bullet" data-glide-dir="=0"></button>
-        <button class="glide__bullet" data-glide-dir="=1"></button>
-        <button class="glide__bullet" data-glide-dir="=2"></button>
-        <button class="glide__bullet" data-glide-dir="=3"></button>
-        <button class="glide__bullet" data-glide-dir="=4"></button>
-        <button class="glide__bullet" data-glide-dir="=5"></button>
+        <button v-for="(image, index) in images" class="glide__bullet" :data-glide-dir="'='+index"></button>
       </div>
     </div>
   </div>
@@ -29,17 +17,38 @@
 
 <script>
   import Glide from '@glidejs/glide'
+  import Portfolio from '../../assets/js/models/Portfolio'
+
+  let glide = new Glide('.glide', {
+    type: 'carousel',
+    perView: 1,
+    gap: 0,
+    autoplay: 4000
+  })
 
   export default {
+    data: function () {
+      return {
+        site: '',
+        images: ''
+      }
+    },
+    beforeCreate() {
+      Portfolio.show(Portfolio.getUrl(),this.$route.params.id)
+        .then(
+          response => {
+            this.site = response.data
+            this.images = JSON.parse(this.site.carousel)
+            setTimeout(function (){
+              glide.mount()
+            })
+          },
+          error => {
+            throw new Error('Show Receiver. ' + error.message)
+          }
+        )
+    },
     mounted () {
-      new Glide('.glide', {
-        type: 'carousel',
-        startAt: 0,
-        perView: 1,
-        focusAt: 'center',
-        gap: 0,
-        autoplay: 4000
-      }).mount()
     }
   }
 </script>
