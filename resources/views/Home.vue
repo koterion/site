13905099928567fd646769234a5d774fa00ef030
koterion <koterion317@gmail.com>
2019-08-14 22:36:12 +0300

    Added animations for eyes

--- resources/views/Home.vue
+++ resources/views/Home.vue
@@ -10,7 +10,7 @@
           erase-style='backspace'
         ></vue-typer></h1>
         <p class="home__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, culpa dolorem deleniti aspernatur itaque! Eos corporis quidem aspernatur quibusdam expedita at dolorem omnis, voluptatem quos labore dolores, saepe voluptatibus debitis!</p>
-        <figure class="home__switcher" @click="light = !light">
+        <figure class="home__switcher" @click="moveEyes">
           <img class="home__switcher--img" src="/img/cat-paw-stroke.svg" alt="">
         </figure>
       </div>
@@ -43,29 +43,58 @@
     components: {
       'vue-typer': VueTyper
     },
-    mounted() {
-      function eyes(event) {
-        const cat = document.querySelector('.lighter__cat')
-        const catEyeLeft = cat.querySelector('.lighter__cat--eye')
-        const catHeight = cat.offsetHeight
-        const catWidth = cat.offsetWidth
-        const catTop = cat.getBoundingClientRect().top
-        const catLeft = cat.getBoundingClientRect().left
-        const position = {
-          x: event.x,
-          y: event.y,
-        }
+    methods: {
+      moveEyes: function () {
+        document.addEventListener("mousemove", mathEyes)
 
-        if ((catLeft + catWidth - position.x) >= 1 && (catLeft + catWidth - position.x) <= catWidth) {
-          catEyeLeft.style.left = Math.abs(catLeft - position.x) + 'px'
-        }
+        this.light = !this.light
+        
+        function mathEyes(event) {
+          const cat = document.querySelector('.lighter__cat')
+          const catEyeLeft = cat.querySelector('.lighter__cat--eye')
+          const catParams = {
+            height: cat.offsetHeight,
+            width: cat.offsetWidth,
+            top: cat.getBoundingClientRect().top,
+            left: cat.getBoundingClientRect().left,
+            eyes: {
+              left: {
+                top: 54,
+                left: 29,
+                width: 16,
+                height: 8
+              }
+            }
+          }
 
-        if ((catTop + catHeight - position.y) >= 1 && (catTop + catHeight - position.y) <= catWidth) {
-          catEyeLeft.style.top = Math.abs(catTop - position.y) + 'px'
+          console.log('play')
+          const position = {
+            x: event.x,
+            y: event.y,
+          }
+          const current = {
+            hor: catParams.left + catParams.eyes.left.left - position.x,  
+            vert: catParams.top + catParams.eyes.left.top - position.y
+          }
+
+          const coef = {
+            y: (catParams.top + catParams.eyes.left.top) / (catParams.eyes.left.height/2),
+            x: (catParams.left + catParams.eyes.left.left) / (catParams.eyes.left.width/2)
+          }
+
+          if ((current.hor + catParams.eyes.left.width) > 0 && (current.hor + catParams.eyes.left.width) <= catParams.eyes.left.width) {
+            catEyeLeft.style.left = current.hor + catParams.eyes.left.left + 'px'
+          } else {
+            catEyeLeft.style.left = -1 * current.hor/coef.x + catParams.eyes.left.left + 'px'
+          }
+
+          if ((current.vert + catParams.eyes.left.height) > 0 && (current.vert + catParams.eyes.left.height) <= catParams.eyes.left.height) {
+            catEyeLeft.style.top = current.vert + catParams.eyes.left.top + 'px'
+          } else {
+            catEyeLeft.style.top = -1 * current.vert/coef.y + catParams.eyes.left.top + 'px'
+          }
         }
       }
-
-      window.onmousemove = eyes
     }
   }
 </script>
